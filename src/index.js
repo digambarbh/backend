beb9aec125c9198a8d4e7b87d2b8eaf10da7f7cd
digambar <digambarb18@gmail.com>
2025-11-13 22:49:02 +0530

    utilities learned

--- src/index.js
+++ src/index.js
@@ -4,7 +4,15 @@ import connectDB from "./db/index.js"
 
 
 connectDB()
+.then(()=>{
+    app.listen(process.env.PORT || 8000,()=>{
+        console.log(`server is running at port :${process.env.PORT}`)
+    })
+})
+.catch((error)=>{
+    console.log("Db connection error ",error)
 
+})
 
 
 
