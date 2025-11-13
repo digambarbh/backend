f1a5023d5314f4e1df5d3f0f7ee636a8c8362cc2
digambar <digambarb18@gmail.com>
2025-11-13 15:14:22 +0530

    databasse connected

---
+++ src/index.js
@@ -0,0 +1,34 @@
+import dotenv from "dotenv";
+dotenv.config();
+import connectDB from "./db/index.js"
+
+
+connectDB()
+
+
+
+
+
+
+// first approach 
+// import express from "express"
+// import { configDotenv } from "dotenv"
+
+// const app=express()
+
+
+// (async()=>{
+//     try{
+//        await mongoose.connect(`${process.env.MONGODB_URL}/${DB_NAME}`)
+//        app.on("error",(error)=>{
+//         console.log("ERROR",error)
+//         throw err
+//        })
+//        app.listen(process.env.PORT,()=>{
+//         console.log(`app is running on port:${process.env.PORT}`)
+//        })
+//     }catch(error){
+//         console.error("ERROR:",error)
+//         throw err
+//     }
+// })()
